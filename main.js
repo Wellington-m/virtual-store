035aaef72e58abe85bef401ecc1ce7c104533c5e
Wellington-m <wellingtonmachioni@gmail.com>
2025-05-07 16:19:52 -0300

    feat: add icon toggle on search button click

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 $(() => {
     $("#search_button").on("click", () => {
+        const src_value = $("#search_button img").attr("src");
+        if (src_value === "images/Search.svg") {
+            $("#search_button img").fadeOut(300, function () {
+                $(this).attr("src", "images/Close_search.svg").fadeIn(300);
+                $(this).attr("alt", "Simbolo de X");
+            });
+        } else {
+            $("#search_button img").fadeOut(300, function () {
+                $(this).attr("src", "images/Search.svg").fadeIn(300);
+                $(this).attr("alt", "Pesquisar");
+            });
+        }
+
         $("#srch_field").slideToggle();
     });
 });
