dca46bf7a7b3b0764f6e77857f14359e6b3ec617
Wellington-m <wellingtonmachioni@gmail.com>
2025-05-21 15:58:17 -0300

    refactor: swich buttom img to use material-symbols-outlined

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 $(() => {
     $("#search_button").on("click", () => {
-        const src_value = $("#search_button img").attr("src");
-        if (src_value === "images/Search.svg") {
-            $("#search_button img").fadeOut(300, function () {
-                $(this).attr("src", "images/Close_search.svg").fadeIn(300);
-                $(this).attr("alt", "Simbolo de X");
+        const value = $("#search_button span").text().replace(/\s+/g, "");
+
+        if (value === "search") {
+            $("#search_button span").fadeOut(300, function () {
+                $(this).text("close").fadeIn(300);
             });
         } else {
-            $("#search_button img").fadeOut(300, function () {
-                $(this).attr("src", "images/Search.svg").fadeIn(300);
-                $(this).attr("alt", "Pesquisar");
+            $("#search_button span").fadeOut(300, function () {
+                $(this).text("search").fadeIn(300);
             });
         }
 
